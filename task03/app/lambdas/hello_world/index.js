5c1c24dd25548ed997292d56c469107d220cd76a
Ankit Kumar <ankit_kumar4@epam.com>
2025-03-27 11:48:03 +0530

    final-commit-3

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,8 +1,6 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+    return {
         statusCode: 200,
-        message: "Hello from Lambda",
+        body: JSON.stringify({ message: "Hello from Lambda" })
     };
-    return response;
-};
+}
