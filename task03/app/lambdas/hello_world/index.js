9871e8c0dcdb77593d097e4fe5562ddae6759010
Ankit Kumar <ankit_kumar4@epam.com>
2025-03-27 12:33:14 +0530

    final-commit

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello from Lambda" }),
+        body: JSON.stringify({ statusCode: 200, message: "Hello from Lambda" }),
         headers: { "Content-Type": "application/json" }
     };
 };
